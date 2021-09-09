1b42141e3fa5f2909cd273c0e3e36a63b9501fed
kramer04 <duncan4031@gmail.com>
2021-09-09 09:09:03 +0200

    Add files via upload
    re write all code

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include "Fenetre.h"
-#include <gtkmm-3.0/gtkmm/application.h>
+#include <gtkmm/application.h>
 
 int main(int argc, char *argv [])
 {
-  if (std::filesystem::exists("out.txt"))
-  {
-    std::filesystem::remove("out.txt");
-  }
   auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
-  Fenetre fenetre;
 
+  Fenetre fenetre;
+  fenetre.delete_unmount_point();
+
+  //Shows the window and returns when it is closed.
   return app->run(fenetre);
 }
